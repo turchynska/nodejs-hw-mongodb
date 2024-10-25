f2cd8a09cabaf5b043349cce5448d995f60791ed
Kate <159571202+turchynska@users.noreply.github.com>
2024-10-25 12:59:01 -0400

    push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { setupServer } from "./server";
+import { setupServer } from './server.js';
+import { initMongoDB } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  await setupServer();
+  initMongoDB();
+};
+bootstrap();
