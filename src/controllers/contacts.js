091167c903f10b49a399af43cb962c8bb05ca313
Kate <159571202+turchynska@users.noreply.github.com>
2024-11-05 08:30:29 -0500

    push

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,10 +7,14 @@ import {
   deleteContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
+
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
+
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -37,7 +41,7 @@ export const getContactIdController = async (req, res, next) => {
 };
 
 export const addContactController = async (req, res) => {
-  const contact = await createContact(req.body);
+  const contact = await addContact(req.body);
 
   res.status(201).json({
     status: 201,
@@ -45,6 +49,7 @@ export const addContactController = async (req, res) => {
     data: contact,
   });
 };
+
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
